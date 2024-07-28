f1487fba26e7aa4927e172feca65183ba9262584
Nin9 <504362292@qq.com>
2024-07-28 19:28:14 +0800

    V2.3.1

--- SteamFunctionAndUiOptimization.js
+++ SteamFunctionAndUiOptimization.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam功能和界面优化
 // @namespace    https://github.com/XuJunxu/steam-script
-// @version      2.3.0
+// @version      2.3.1
 // @description  Steam功能和界面优化
 // @author       Nin9
 // @iconURL      https://store.steampowered.com/favicon.ico
